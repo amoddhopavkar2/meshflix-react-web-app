805fdd5a08caaac41227b97d4ca0d8b82c6da07b
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-31 16:42:33 -0400

    finish navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return () => (window.onscroll = null);
+  }; 
+
   return (
-    <div className="wd-navbar">
+    <div className={isScrolled ? "wd-navbar scrolled" : "wd-navbar"}>
       <div className="wd-container">
         <div className="wd-left">
           <img
@@ -17,14 +25,21 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="wd-right">
-          <Search className="wd-icons"/>
+          <Search className="wd-icons" />
           <span>KID</span>
-          <Notifications className="wd-icons"/>
+          <Notifications className="wd-icons" />
           <img
             src="https://media.tenor.com/BrD7r8OL4hkAAAAC/psyduckpc.gif"
             alt="Profile Picture"
           />
-          <ArrowDropDown className="wd-icons"/>
+
+          <div className="wd-profile-container">
+            <ArrowDropDown className="wd-icons" />
+            <div className="wd-profile-options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
