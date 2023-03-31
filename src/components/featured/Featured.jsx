7d181e22de5939ae69cde18defd944448aa295d4
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-31 17:27:46 -0400

    start featured

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import "./featured.scss"
+
+const Featured = () => {
+  return (
+    <div className="wd-featured">
+        <img
+            src="https://media.tenor.com/BrD7r8OL4hkAAAAC/psyduckpc.gif"
+            alt="Profile Picture"
+          />
+    </div>
+  )
+}
+
+export default Featured
