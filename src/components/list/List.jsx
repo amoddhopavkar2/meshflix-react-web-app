891fe12416986aa32e968dc16c79e33bfac514db
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-31 20:50:35 -0400

    finished recommendations list

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -4,28 +4,37 @@ import {
 } from "@material-ui/icons";
 import "./list.scss";
 import ListItem from "../listItem/ListItem";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const listReference = useRef();
 
-    const listReference = useRef()
-
-    const handleClick = (direction) => {
-        let distance = listReference.current.getBoundingClientRect().x - 50;
-        if (direction === "left") {
-            listReference.current.style.transform = `translateX(${230 + distance}px)`
-        }
-        else if (direction === "right") {
-            listReference.current.style.transform = `translateX(${-230 + distance}px)`
-        }
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listReference.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listReference.current.style.transform = `translateX(${230 + distance}px)`;
+    } else if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listReference.current.style.transform = `translateX(${
+        -230 + distance
+      }px)`;
     }
+  };
 
   return (
     <div className="wd-list-container">
       <span className="wd-list-title">Continue to watch</span>
       <div className="wd-list-wrapper">
-        <ArrowBackIosOutlined className="wd-slider-arrow left" onClick={() => handleClick("left")} />
-        <div className="wd-list-item-container" ref={listReference} >
+        <ArrowBackIosOutlined
+          className="wd-slider-arrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="wd-list-item-container" ref={listReference}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -41,9 +50,11 @@ const List = () => {
           <ListItem />
           <ListItem />
           <ListItem />
-          
         </div>
-        <ArrowForwardIosOutlined className="wd-slider-arrow right" onClick={() => handleClick("right")} />
+        <ArrowForwardIosOutlined
+          className="wd-slider-arrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
