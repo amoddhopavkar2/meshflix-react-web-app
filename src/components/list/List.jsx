49f8f8d36a0de1c99998b2f0168771ca3b9698fd
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-31 21:14:53 -0400

    need to fix hovering position

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -35,21 +35,21 @@ const List = () => {
           style={{ display: !isMoved && "none" }}
         />
         <div className="wd-list-item-container" ref={listReference}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index = {0} />
+          <ListItem index = {1}  />
+          <ListItem index = {2}  />
+          <ListItem index = {3}  />
+          <ListItem index = {4}  />
+          <ListItem index = {5}  />
+          <ListItem index = {6}  />
+          <ListItem index = {7}  />
+          <ListItem index = {8}  />
+          <ListItem index = {9}  />
+          <ListItem index = {10}  />
+          <ListItem index = {11}  />
+          <ListItem index = {12}  />
+          <ListItem index = {13}  />
+          <ListItem index = {14}  />
         </div>
         <ArrowForwardIosOutlined
           className="wd-slider-arrow right"
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,14 +1,48 @@
-import "./listItem.scss"
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import "./listItem.scss";
+import { useState } from "react";
 
-const ListItem = () => {
+const ListItem = (index) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="wd-list-item">
-        <img
+    <div
+      className="wd-list-item"
+      style={{left: isHovered && index * 225 - 50}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
       />
-    </div>
-  )
-}
+      <div className="wd-item-info-container">
+        <div className="wd-icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
+        </div>
 
-export default ListItem
+        <div className="wd-item-info-top">
+          <span>1 hour 14 mins</span>
+          <span className="wd-age-limit-container">+16</span>
+          <span>1999</span>
+        </div>
+
+        <div className="wd-item-desc">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem porro nobis iure labore impedit reprehenderit.
+        </div>
+
+        <div className="wd-item-genre">Action</div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
