180ccb35a7fab302225c7d132a50d3647c09df5f
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-31 21:57:56 -0400

    hovering position still needs word

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -9,10 +9,12 @@ import { useState } from "react";
 
 const ListItem = (index) => {
   const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
     <div
       className="wd-list-item"
-      style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -20,12 +22,16 @@ const ListItem = (index) => {
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
       />
+      {isHovered && (
+      <>
+      <video src={trailer} autoPlay={true} loop />
+
       <div className="wd-item-info-container">
-        <div className="wd-icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
+        <div className="wd-icons-container">
+          <PlayArrow className="wd-icons"/>
+          <Add className="wd-icons"/>
+          <ThumbUpAltOutlined className="wd-icons"/>
+          <ThumbDownAltOutlined className="wd-icons"/>
         </div>
 
         <div className="wd-item-info-top">
@@ -41,6 +47,7 @@ const ListItem = (index) => {
 
         <div className="wd-item-genre">Action</div>
       </div>
+      </>)}
     </div>
   );
 };
